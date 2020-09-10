4674aa661ebf2c99cbde0f5caeba39900fe3deb0
Skarlaks71 <mapisaro07@gmail.com>
2020-09-10 17:28:15 -0300

    Create Tetromios and SpawnPeaces

---
+++ 3D-Tetris-Game-test-kokku/Assets/Tetris/Scripts/Manager/SpawnManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+
+	public GameObject[] peaces;
+	private bool _isGround = true;
+
+	// Update is called once per frame
+	void Update () {
+		if (_isGround)
+		{
+			_isGround = false;
+			SpawnPeace();
+		}
+	}
+	private void SpawnPeace()
+	{
+		Instantiate(peaces[Random.Range(0, peaces.Length)],new Vector3(0,13,0),Quaternion.identity);
+	}
+}
